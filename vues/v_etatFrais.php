140ab670668b95aa206acebf8d79a98097f4b683
KilianBrd <kilianberaud3@gmail.com>
2023-02-02 16:25:47 +0100

    Le comentaire

--- vues/v_etatFrais.php
+++ vues/v_etatFrais.php
@@ -38,7 +38,8 @@
              <tr>
                 <th class="date">Date</th>
                 <th class="libelle">Libellé</th>
-                <th class='montant'>Montant</th>                
+                <th class='montant'>Montant</th>
+                <th class="moyen">Moyen de paiement</th>                
              </tr>
         <?php      
           foreach ( $lesFraisHorsForfait as $unFraisHorsForfait ) 
@@ -46,11 +47,13 @@
 			$date = $unFraisHorsForfait['date'];
 			$libelle = $unFraisHorsForfait['libelle'];
 			$montant = $unFraisHorsForfait['montant'];
+      $libPaiement = $unFraisHorsForfait['libPaiement'];
 		?>
              <tr>
                 <td><?php echo $date ?></td>
                 <td><?php echo $libelle ?></td>
                 <td><?php echo $montant ?></td>
+                <td><?php echo $libPaiement?></td>
              </tr>
         <?php 
           }
